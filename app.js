fdb5b22b1205b10c8a65a55fefa3121760449adc
Dezolutions <mars92851@gmail.com>
2021-12-06 00:17:27 +0200

    assignment 1

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
-  }
+  },
+  methods: {
+    add(num) {
+      this.counter += num
+    },
+    reduce(num) {
+      this.counter -= num
+    },
+    setName(event, lastName) {
+      this.name = `${event.target.value} ${lastName}`
+    },
+    submitForm(e) {
+      
+    },
+    confirmName() {
+      this.confirmedName = this.name
+    },
+    
+  },
 });
 
 app.mount('#events');
